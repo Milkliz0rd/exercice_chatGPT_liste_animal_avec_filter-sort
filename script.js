c43fd1d51c43ee445717614d439c6c4187ea2325
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-10-04 14:31:00 +0200

    Ajout des animaux à un tableau

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const sectionAnimal = document.querySelector(".titre");
-console.log(sectionAnimal);
+//ajout du titre en JS pour m'exercer et voir si JS était bien relié
 
+const sectionAnimal = document.querySelector(".titre");
 const titreAnimal = document.createElement("h1");
-console.log(titreAnimal);
 titreAnimal.innerText = "Les animaux"
 sectionAnimal.appendChild(titreAnimal);
+
+//ajout de la liste des animaux
+let animauxTableau = [];
+const animalInput = document.querySelector("#animal-input");
+const animalAdd = document.querySelector("#add-animal");
+const animalList = document.querySelector("#animal-list")
+
+function ajouterAnimal () {
+    const valeur = animalInput.value.trim();
+    if (valeur !== "") {
+        animauxTableau.push(valeur);
+        animalInput.value = "";
+    }else{
+        alert("veuillez entrer un nom d'animal")
+    }
+    
+}
+
+animalAdd.addEventListener("click", ajouterAnimal);
+console.log(animauxTableau);
+
