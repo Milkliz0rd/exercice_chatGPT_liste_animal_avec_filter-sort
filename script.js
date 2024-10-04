86e85096934cd9a4149e97a91ccb03f88aebd57b
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-10-04 15:34:15 +0200

    mise en place de l'exercice

--- script.js
+++ script.js
@@ -5,23 +5,3 @@ const titreAnimal = document.createElement("h1");
 titreAnimal.innerText = "Les animaux"
 sectionAnimal.appendChild(titreAnimal);
 
-//ajout de la liste des animaux
-let animauxTableau = [];
-const animalInput = document.querySelector("#animal-input");
-const animalAdd = document.querySelector("#add-animal");
-const animalList = document.querySelector("#animal-list")
-
-function ajouterAnimal () {
-    const valeur = animalInput.value.trim();
-    if (valeur !== "") {
-        animauxTableau.push(valeur);
-        animalInput.value = "";
-    }else{
-        alert("veuillez entrer un nom d'animal")
-    }
-    
-}
-
-animalAdd.addEventListener("click", ajouterAnimal);
-console.log(animauxTableau);
-
